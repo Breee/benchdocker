bb9efb1f29db8ef8610bfca7dbf0b704e6cd15b3
Breee <loeffler.uni@gmail.com>
2019-07-19 10:12:23 +0200

    currently collexted svcomp features 5691 (Benchmark still running)

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
-cd /ultimate/releaseScripts/default/UAutomizer-linux && benchexec -d -o /output /input/student-heuristics-extract-features.xml
+git clone https://github.com/sosy-lab/sv-benchmarks /svcomp
+#cd /ultimate/releaseScripts/default/UAutomizer-linux && benchexec -d -o /output /input/student-heuristics-extract-features.xml
+cd /ultimate/releaseScripts/default/UAutomizer-linux && benchexec -d -o /output /input/student-svcomp-extract-features.xml
+
